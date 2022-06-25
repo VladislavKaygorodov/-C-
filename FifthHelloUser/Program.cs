1531fd7cdb3f15a91ed06a8884238910ee61d92a
Vladislav <kaygorodov.vlad@gmail.com>
2022-06-25 13:42:52 +0700

    Особое приветствие

---
+++ FifthHelloUser/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "влад")
+{
+    Console.WriteLine("Ура, это же Влад!");
+}
+else
+{
+    Console.Write("Привет, ", username);
+    Console.Write(username);
+}
